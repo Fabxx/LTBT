6e5fab1fc65ae9ab47ca0fcd27407e0d7025d459
Fabx <30447649+Fabxx@users.noreply.github.com>
2024-09-28 13:08:58 +0200

    update script with message info

--- noCPUBoost.sh
+++ noCPUBoost.sh
@@ -4,6 +4,10 @@ status=$(cat "/sys/devices/system/cpu/intel_pstate/no_turbo")
 
 if [[ $status -eq 0 ]] then echo "1" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 
+echo "turbo boost disabled"
+
 else echo "0" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+
+echo "turbo boost enabled"
 
 fi
